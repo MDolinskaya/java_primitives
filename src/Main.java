fb4d2283fc3f14cf6189ed00c2aa96e39d2493f2
MartaD <mmdolinskaya@gmail.com>
2024-06-30 22:31:13 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int price = 34350;
+        int oneMile = 20;
+
+        int miles = price / oneMile;
+
+        System.out.println(miles);
+    }
+}
