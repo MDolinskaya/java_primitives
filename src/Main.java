68e7ef2c5f529c9ce790c4f19f9c8cb387062971
MartaD <mmdolinskaya@gmail.com>
2024-07-01 10:53:32 +0300

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int price = 34350;
         int oneMile = 20;
-
         int miles = price / oneMile;
-
         System.out.println(miles);
     }
 }
